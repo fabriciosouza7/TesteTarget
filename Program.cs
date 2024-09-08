f5990cb8e078cbde86c67af6e12aad3d1b08acbd
Fabricio de Souza <110771040+fabriciosouza7@users.noreply.github.com>
2024-09-08 14:52:19 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{  // 1)
+
+    static void Main()
+    {
+        Console.Write("Informe um número: ");
+        if (int.TryParse(Console.ReadLine(), out int numero))
+        {
+            bool pertence = VerificaFibonacci(numero);
+            // Exiba o resultado
+            if (pertence)
+            {
+                Console.WriteLine($"O {numero} pertence à sequência de Fibonacci.");
+            }
+            else
+            {
+                Console.WriteLine($"O {numero} não pertence à sequência de Fibonacci.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida. Informe um número inteiro.");
+        }
+    }
+
+    static bool VerificaFibonacci(int numero)
+    {
+        int fib1 = 0;
+        int fib2 = 1;
+
+        if (numero == fib1 || numero == fib2)
+        {
+            return true;
+        }
+
+        int fibAtual = fib1 + fib2;
+        while (fibAtual <= numero)
+        {
+            if (fibAtual == numero)
+            {
+                return true;
+            }
+            fib1 = fib2;
+            fib2 = fibAtual;
+            fibAtual = fib1 + fib2;
+        }
+
+        // Se o número não foi encontrado na sequência, retorne falso
+        return false;
+    }
+}
+
+
